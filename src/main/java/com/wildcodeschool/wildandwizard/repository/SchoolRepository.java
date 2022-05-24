df9f522566f5456990166b71c5e33a90543b8d08
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-05-24 15:51:37 +0200

    SchoolController CRUD operators

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,10 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 // TODO : update this interface
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import entity.Wizard;  
 
-public interface SchoolRepository {
+@Repository
+public interface SchoolRepository extends JpaRepository<Wizard, Long>  {
 }
